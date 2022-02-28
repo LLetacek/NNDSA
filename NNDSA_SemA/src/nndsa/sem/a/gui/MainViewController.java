de2c5dc7d8e7e5257b400b0d70ba947a81db69c9
LLetacek <72096612+LLetacek@users.noreply.github.com>
2022-02-28 10:54:19 +0100

    🚧 GUI

--- NNDSA_SemA/src/nndsa/sem/a/gui/MainViewController.java
+++ NNDSA_SemA/src/nndsa/sem/a/gui/MainViewController.java
@@ -2,6 +2,8 @@ package nndsa.sem.a.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,6 +12,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import nndsa.sem.a.railway.RailwayTrackType;
 import nndsa.sem.a.railway.SimpleRailway;
 
 /**
@@ -22,17 +25,17 @@ public class MainViewController implements Initializable {
     @FXML
     private TableView<SimpleRailway> tbViewRailway;
     @FXML
-    private TableColumn<?, ?> VRKey;
+    private TableColumn<SimpleRailway, String> VRKey;
     @FXML
-    private TableColumn<?, ?> VRLength;
+    private TableColumn<SimpleRailway, Integer> VRLength;
     @FXML
-    private TableColumn<?, ?> VROccupancy;
+    private TableColumn<SimpleRailway, Integer> VROccupancy;
     @FXML
-    private TableColumn<?, ?> VRType;
+    private TableColumn<SimpleRailway, RailwayTrackType> VRType;
     @FXML
-    private TableView<?> tbViewNeighbor;
+    private TableView<String> tbViewNeighbor;
     @FXML
-    private TableColumn<?, ?> VNKey;
+    private TableColumn<String, String> VNKey;
     @FXML
     private TextField txtFieldKey;
     @FXML
@@ -40,7 +43,7 @@ public class MainViewController implements Initializable {
     @FXML
     private TextField txtFieldOccupancy;
     @FXML
-    private ChoiceBox<?> txtFieldType;
+    private ChoiceBox<RailwayTrackType> txtFieldType;
     @FXML
     private TextField txtFieldNeighborsKey;
     @FXML
@@ -52,6 +55,9 @@ public class MainViewController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<RailwayTrackType> options = FXCollections.observableArrayList(RailwayTrackType.values());
+        txtFieldType.getItems().addAll(options);
+        txtFieldType.setValue(RailwayTrackType.DIRECT);
         // TODO
     }    
 
