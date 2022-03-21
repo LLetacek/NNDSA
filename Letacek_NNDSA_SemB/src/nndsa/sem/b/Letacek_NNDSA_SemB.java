d70c21d0e7d2fbb782d8c654572445f92a742f7d
LLetacek <72096612+LLetacek@users.noreply.github.com>
2022-03-22 00:26:32 +0100

    🎉 init

---
+++ Letacek_NNDSA_SemB/src/nndsa/sem/b/Letacek_NNDSA_SemB.java
@@ -0,0 +1,16 @@
+package nndsa.sem.b;
+
+/**
+ *
+ * @author ludek
+ */
+public class Letacek_NNDSA_SemB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
