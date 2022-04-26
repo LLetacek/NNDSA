ec91f82f9b343c28c0c9a519002657ccf5fdd18e
LLetacek <72096612+LLetacek@users.noreply.github.com>
2022-04-27 00:12:15 +0200

    🔧 Fix statistics

--- Letacek_NNDSA_SemC/src/nndsa/sem/c/gui/MainViewController.java
+++ Letacek_NNDSA_SemC/src/nndsa/sem/c/gui/MainViewController.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.stream.Stream;
 import javafx.collections.FXCollections;
@@ -37,19 +39,30 @@ public class MainViewController implements Initializable {
     private Serialization serialization;
     private final ObservableList<String> wordStatistics = FXCollections.observableArrayList();
     private final String outBinaryFile = "./out";
+    private List<Integer> statsBinary;
+    int binarySum = 0;
+    int binaryMin = Integer.MAX_VALUE;
+    int binaryMax = Integer.MIN_VALUE;
+    private List<Integer> statsInterpolation;
+    int interpolationSum = 0;
+    int interpolationMin = Integer.MAX_VALUE;
+    int interpolationMax = Integer.MIN_VALUE;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         searchEngine = new SearchEngine();
         serialization = new Serialization();
         list.setItems(wordStatistics);
-    }    
+        statsBinary = new LinkedList<>();
+        statsInterpolation = new LinkedList<>();
+    }     
 
 
     @FXML
     private void find(ActionEvent event) {
         clear(event);
         search(txtWord.getText(), outBinaryFile);
+        clearStats();
     }
 
     @FXML
@@ -71,6 +84,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     private void findFromFile(ActionEvent event) throws IOException {
+        clearStats();
         clear(event);
         File file = getFile("txt", "*.txt");
 
@@ -81,6 +95,35 @@ public class MainViewController implements Initializable {
         try (Stream<String> fileStream = Files.lines(file.toPath(), StandardCharsets.UTF_8)) {
                 fileStream.filter(t -> t != null).forEach(((line) -> { search(line, outBinaryFile); }));
         }
+        
+        printStats();
+    }
+    
+    private void clearStats() {
+        statsBinary.clear();
+        binarySum = 0;
+        binaryMin = Integer.MAX_VALUE;
+        binaryMax = Integer.MIN_VALUE;
+        statsInterpolation.clear();
+        interpolationSum = 0;
+        interpolationMin = Integer.MAX_VALUE;
+        interpolationMax = Integer.MIN_VALUE;
+    }
+    
+    private void printStats() {
+        wordStatistics.add(0, "---------------");
+        statsBinary.forEach((t) -> {
+            binarySum+=t;
+            if (t<binaryMin) binaryMin = t;
+            if (t>binaryMax) binaryMax = t;
+        });
+        wordStatistics.add(0, "BINARY\t\t\t AVG:" + binarySum/statsBinary.size() +  "\tMIN:" + binaryMin + "\tMAX:" + binaryMax);
+        statsInterpolation.forEach((t) -> {
+            interpolationSum+=t; 
+            if (t<interpolationMin) interpolationMin = t;
+            if (t>interpolationMax) interpolationMax = t;
+        });
+        wordStatistics.add(0, "INTERPOLATION\t AVG:" + interpolationSum/statsInterpolation.size() +  "\tMIN: " + interpolationMin + "\tMAX: " + interpolationMax);
     }
 
     private File getFile(String description, String... extensions) {
@@ -96,11 +139,13 @@ public class MainViewController implements Initializable {
         Pair<Integer, Word> finder;
         try {
             finder = searchEngine.findInBinaryBase(word, outBinaryFile,SearchType.INTERPOLATION);
+            statsInterpolation.add(finder.getKey());
             wordStatistics.add("INTERPOLATION:\t" + finder.getKey() + "\t: " + 
                     finder.getValue().getKey() + " | " + 
                     finder.getValue().getEnglishWord() + " | " + 
                     finder.getValue().getGermanWord());
             finder = searchEngine.findInBinaryBase(word, outBinaryFile,SearchType.BINARY);
+            statsBinary.add(finder.getKey());
             wordStatistics.add("BINARY:\t\t\t" + finder.getKey() + "\t: " + finder.getValue().getKey());
             wordStatistics.add("------------");
         } catch (Exception ex) {
@@ -111,5 +156,6 @@ public class MainViewController implements Initializable {
     @FXML
     private void clear(ActionEvent event) {
         wordStatistics.clear();
+        clearStats();
     }
 }
